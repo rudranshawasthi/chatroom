c7f642f3b52f09cc180c24376f4a2830c962dcc0
rudranshawasthi <rudranshofficial@gmail.com>
2021-05-25 15:29:36 +0530

    Auth

---
+++ Chatroom/lib/auth/signup.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  String email = "";
+  String password = "";
+
+  void signUpAction() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void signInAction() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        elevation: 8,
+        title: Text("Sign Up"),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: new Form(
+            child: Column(
+          children: [
+            TextFormField(
+              onChanged: (val) => setState(() {
+                email = val;
+              }),
+            ),
+            TextFormField(
+              onChanged: (val) => setState(() {
+                password = val;
+              }),
+            ),
+            RaisedButton(
+              onPressed: () async {
+                signUpAction();
+              },
+              child: Text("Signup"),
+            ),
+            RaisedButton(
+              onPressed: () async {
+                signInAction();
+              },
+              child: Text("SignIN"),
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
